fffb969a82db3aa0bb18d9da98c1f1463b1fdbf2
Crowdbotics <team@crowdbotics.com>
2020-01-08 10:52:06 +0000

    Update models

--- home/migrations/0002_homepage_testdata.py
+++ home/migrations/0002_homepage_testdata.py
@@ -6,13 +6,13 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('home', '0001_initial'),
+        ("home", "0001_initial"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='homepage',
-            name='testdata',
+            model_name="homepage",
+            name="testdata",
             field=models.CharField(blank=True, max_length=356, null=True),
         ),
     ]
--- home/models.py
+++ home/models.py
@@ -24,6 +24,7 @@ class CustomText(models.Model):
 class HomePage(models.Model):
     body = models.TextField()
     testdata = models.CharField(max_length=356, null=True, blank=True,)
+    testdatademo = models.CharField(max_length=9000, null=True, blank=True,)
 
     @property
     def api(self):
