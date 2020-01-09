aacbe8ebeab34ca4199f11d8e87e92b92ac60756
Crowdbotics <team@crowdbotics.com>
2020-01-09 06:33:53 +0000

    Update models

--- home/api/v1/viewsets.py
+++ home/api/v1/viewsets.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets
 from rest_framework import authentication
-from .serializers import CustomTextSerializer, HomePageSerializer
+from .serializers import CustomTextSerializer, HomePageSerializer, TestSerializer
 import json
 
 from django import apps
@@ -22,7 +22,7 @@ from home.api.v1.serializers import (
     HomePageSerializer,
     UserSerializer,
 )
-from home.models import CustomText, HomePage
+from home.models import CustomText, HomePage, Test
 
 
 class SignupViewSet(ModelViewSet):
@@ -89,3 +89,12 @@ class AppReportView(APIView):
             {"models": self._get_models(), "urls": self._get_urls()},
             status=status.HTTP_200_OK,
         )
+
+
+class TestViewSet(viewsets.ModelViewSet):
+    serializer_class = TestSerializer
+    authentication_classes = (
+        authentication.SessionAuthentication,
+        authentication.TokenAuthentication,
+    )
+    queryset = Test.objects.all()
--- home/models.py
+++ home/models.py
@@ -33,3 +33,8 @@ class HomePage(models.Model):
     @property
     def field(self):
         return "body"
+
+
+class Test(models.Model):
+    "Generated Model"
+    test = models.TextField()
