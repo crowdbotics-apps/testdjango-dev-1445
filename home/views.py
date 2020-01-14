96bb0e65f792351bdb69ddf5a9395c2649a43003
Crowdbotics <team@crowdbotics.com>
2020-01-14 05:49:19 +0000

    remove package chainerboard

--- home/views.py
+++ home/views.py
@@ -6,7 +6,6 @@ from .models import Test, Table, TestDemo, CustomText, HomePage, Testdemo1
 def home(request):
     packages = [
 	{'name':'chainer-cuda-deps', 'url': 'http://pypi.python.org/pypi/chainer-cuda-deps/1.1.0.1'},
-	{'name':'chainerboard', 'url': 'http://pypi.python.org/pypi/chainerboard/0.1.5'},
         {
             "name": "django-allauth",
             "url": "https://pypi.org/project/django-allauth/0.38.0/",
