65f8bf6e3398c2997f121861e450621eeb116c9f
Crowdbotics <team@crowdbotics.com>
2020-01-14 06:07:35 +0000

    remove package boto3

--- home/views.py
+++ home/views.py
@@ -5,7 +5,6 @@ from .models import Test, Table, TestDemo, CustomText, HomePage, Testdemo1
 
 def home(request):
     packages = [
-	{'name':'boto3', 'url': 'http://pypi.python.org/pypi/boto3/1.5.15'},
         {
             "name": "django-allauth",
             "url": "https://pypi.org/project/django-allauth/0.38.0/",
